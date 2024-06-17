8c25bb1a65b3700478f1030306be806cc1d329e5
lijinfei <1638546595@qq.com>
2024-06-17 10:14:05 +0800

    homework1 字母出现顺序

--- day02/homework/chat/src/main.rs
+++ day02/homework/chat/src/main.rs
@@ -4,5 +4,18 @@ fn main() {
     println!("请输入任意字符!");
     let mut str = String::new();
     io::stdin().read_line(&mut str).expect("请输入正确的输入");
-    let str = str.trim().split("");
+    let str = str.trim().to_lowercase();
+    let mut result = [0u32; 26];
+    for s in str.chars() {
+        let char_code = s as u8;
+        // println!("{s} => {char_code}");
+        if char_code >= 97 && char_code <= 122 {
+            result[(char_code - 97) as usize] = result[(char_code - 97) as usize] + 1;
+        }
+    }
+    // println!("{:?}", result);
+    for code in 0..26 {
+        let char_code = code + 97 as u8;
+        println!("{} => 出现了 {} 次", char_code as char, result[code as usize]);
+    }
 }
