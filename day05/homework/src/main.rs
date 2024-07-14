fede4683b03d2c21edf8e1290a52f7be7185070a
lijinfei <1638546595@qq.com>
2024-07-14 21:58:23 +0800

    多种方式计算

--- day05/homework/src/main.rs
+++ day05/homework/src/main.rs
@@ -1,4 +1,4 @@
-use std::{sync::{Arc, Mutex}, thread};
+use std::{sync::{Arc, Mutex, mpsc}, thread, io::stdin};
 
 
 const NUM_POINTS: u64 = 100000;
@@ -9,6 +9,22 @@ fn main() {
     // 多线程+线程间共享可变数据的模式
     // 多线程 +mpsc 模式
     // 以上两种模式任选其一或两者都做
+
+    // 多线程
+    let mut choice = String::new();
+    println!("输入1 根据多线程计算");
+    println!("输入2 根据管道通信计算");
+    stdin().read_line(&mut choice).unwrap();
+
+    match choice.trim() {
+        "1" => thread_fn(),
+        "2" => channel_fn(),
+        _ => println!("Unknown")
+    }
+    
+}
+
+fn thread_fn() {
     let mut threads = vec![];
     let result: Arc<Mutex<Vec<u64>>> = Arc::new(Mutex::new(vec![]));
 
@@ -35,11 +51,38 @@ fn main() {
     let result = result.lock().unwrap();
 
     println!("100w 以内的质数: {:?}", result);
-
-
-    
 }
 
+fn channel_fn() {
+    let mut threads = vec![];
+
+    let (sender, receiver) = mpsc::channel::<Vec<u64>>();
+    let _logger_thread = thread::spawn(move || {
+        while let Ok(s) = receiver.recv() {
+            println!("100w 以内的质数: {:?}", s);
+        }
+    });
+
+    for i in 0..NUM_THREADS {
+        let tx = sender.clone();
+        let handler = thread::spawn(move || {
+            let mut local_result = vec![];
+            let start = i * NUM_POINTS;
+            let end = i * NUM_POINTS + NUM_POINTS;
+            for j in start ..end {
+                if is_prima(j) {
+                    local_result.push(j);
+                }
+            }
+            let _ =tx.send(local_result);
+            
+        });
+        threads.push(handler);
+    }
+    for tr in threads {
+        tr.join().unwrap();
+    }
+}
 
 
 fn is_prima(num: u64) -> bool {
